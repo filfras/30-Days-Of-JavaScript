53311f354b8204ffd312f2ebf58233aed9126759
Filipa <118170276+filfras@users.noreply.github.com>
2023-01-27 18:39:16 +0000

    Update main.js

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -143,3 +143,50 @@ let numbersArray = [number];
 
 return numbersArray.join(' ')
 }
+
+//12.Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+stringBecause = "You cannot end a sentence with because because because is a conjunction"
+console.log(stringBecause.substr(31, 23))
+
+//EXERCISE 3
+
+//1.'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+
+const stringLove = "Love is the best thing in this world. Some found their love and some are still looking for their love."
+const stringLoveCount = stringLove.split(' ');
+
+const loveCount = /love/gi
+
+console.log(stringLoveCount.length, "how many words in the full sentence")
+console.log(stringLove.match(loveCount).length, "how many times love appears")
+
+//2.Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+const becauseString = "You cannot end a sentence with because because because is a conjunction."
+const becauseCount = /because/gi
+console.log(becauseString.match(becauseCount).length, "how many times because appears")
+
+//3.Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+
+const cleanSentence = sentence.replace(/%|#|@|\$|;|&/gi, '')
+console.log(cleanSentence, "//: clean sentence without the special characters")
+
+//UNDER CONSTRUCTION
+const sentenceArray = cleanSentence.split(' ');
+console.log(sentenceArray, "//: sentence array")
+
+function frequentWords (array) {
+    let count = 0;
+    let wordAndCount = ''
+    for (let i = 0; i < array.length; i++) {
+        count = array.match(/array[i]/g)
+        wordAndCount = `${array[i]}: ${count} times`
+        return console.log(wordAndCount)
+    }
+}
+
+frequentWords(sentenceArray)
+
+
+
+
